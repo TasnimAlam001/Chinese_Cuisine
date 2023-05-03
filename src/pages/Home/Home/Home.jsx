6e3137d7a93a75851f04698051c0c308ee74f8f3
tasnimalam001 <tasnimalam.001@gmail.com>
2023-05-03 19:10:06 +0600

    data fetch done from local server

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Home = () => {
+    const [chefs, setChefs]= useState([])
+    useEffect(()=>{
+        fetch('http://localhost:5000/chef')
+        .then(res => res.json())
+        .then(data => setChefs(data))
+        .catch(error => console.error(error))
+    },[])
     return (
         <div>
-            <h2>This is home</h2>
+            {
+
+            }
         </div>
     );
 };
